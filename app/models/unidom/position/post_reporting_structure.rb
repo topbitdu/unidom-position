451543b90201588ea58fb2473d367db265e75596
Topbit Du <topbit.du@gmail.com>
2016-09-04 12:14:55 +0800

    1, Improve the Post Reporting Structure for the coding style.

--- app/models/unidom/position/post_reporting_structure.rb
+++ app/models/unidom/position/post_reporting_structure.rb
@@ -12,10 +12,10 @@ class Unidom::Position::PostReportingStructure < ActiveRecord::Base
   scope :superior_post_is, ->(superior_post) { where superior_post_id: to_id(superior_post) }
   scope :inferior_post_is, ->(inferior_post) { where inferior_post_id: to_id(inferior_post) }
 
-  def self.report!(superior_post: nil, inferior_post: nil, opened_at: Time.now, elemental: true)
+  def self.report!(superior_post: nil, inferior_post: nil, elemental: true, opened_at: Time.now)
     raise ArgumentError.new('The superior_post argument is required.') if superior_post.blank?
     raise ArgumentError.new('The inferior_post argument is required.') if inferior_post.blank?
-    self.inferior_post_is(inferior_post).superior_post_is(superior_post).valid_at.alive.first_or_create! opened_at: opened_at, elemental: elemental
+    self.inferior_post_is(inferior_post).superior_post_is(superior_post).valid_at.alive.first_or_create! elemental: elemental, opened_at: opened_at
   end
 
 end
