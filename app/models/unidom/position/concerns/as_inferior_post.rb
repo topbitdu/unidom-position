f0bf3c393751add0b803f67a7971dd66878600a6
Topbit Du <topbit.du@gmail.com>
2016-09-11 17:54:49 +0800

    1, Improve the As Superior Post concern for the #is_reported_to! method. 2, Improve the As Superior Post concern to add the #is_reported_to? method. 3, Improve the As Inferior Post concern for the #report_to! method. 4, Improve the As Inferior Post concern to add the #report_to? method. 5, Improve the Road Map file for the v1.4. 6,  Improve the Change Log file for the v1.4. 7, Improve the Read Me file for the source code examples. 8, Migrate the version from v1.3 to v1.4.

--- app/models/unidom/position/concerns/as_inferior_post.rb
+++ app/models/unidom/position/concerns/as_inferior_post.rb
@@ -7,8 +7,12 @@ module Unidom::Position::Concerns::AsInferiorPost
     has_many :superior_post_reporting_structures, class_name: 'Unidom::Position::PostReportingStructure', source: :inferior_post, foreign_key: :inferior_post_id
     has_many :superior_posts,                     through:    :superior_post_reporting_structures,        source: :superior_post
 
-    def report_to!(superior_post, at: Time.now, primary: false)
-      superior_post_reporting_structures.create! superior_post: superior_post, elemental: primary, opened_at: at
+    def report_to!(superior_post, at: Time.now, primary: true)
+      superior_post_reporting_structures.superior_post_is(superior_post).valid_at(now: at).alive.first_or_create! elemental: primary, opened_at: at
+    end
+
+    def report_to?(superior_post, at: Time.now, primary: true)
+      superior_post_reporting_structures.superior_post_is(superior_post).valid_at(now: at).alive.primary(primary).exists?
     end
 
   end
--- app/models/unidom/position/concerns/as_superior_post.rb
+++ app/models/unidom/position/concerns/as_superior_post.rb
@@ -7,8 +7,12 @@ module Unidom::Position::Concerns::AsSuperiorPost
     has_many :inferior_post_reporting_structures, class_name: 'Unidom::Position::PostReportingStructure', source: :superior_post, foreign_key: :superior_post_id
     has_many :inferior_posts,                     through:    :inferior_post_reporting_structures,        source: :inferior_post
 
-    def is_reported_to!(by: nil, at: Time.now, primary: false)
-      inferior_post_reporting_structures.create! inferior_post: by, elemental: primary, opened_at: at
+    def is_reported_to!(by: nil, at: Time.now, primary: true)
+      inferior_post_reporting_structures.inferior_post_is(by).valid_at(now: at).alive.first_or_create! elemental: primary, opened_at: at
+    end
+
+    def is_reported_to?(by: nil, at: Time.now, primary: true)
+      inferior_post_reporting_structures.inferior_post_is(by).valid_at(now: at).alive.primary(primary).exists?
     end
 
   end
