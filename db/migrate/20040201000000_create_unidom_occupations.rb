6b990e31e3b9d9595dedaf9e94130d81a6a0453c
Topbit Du <topbit.du@gmail.com>
2016-11-27 21:04:00 +0800

    1, Improve the Occupation migration for the coding style.

--- db/migrate/20040201000000_create_unidom_occupations.rb
+++ db/migrate/20040201000000_create_unidom_occupations.rb
@@ -14,7 +14,7 @@ class CreateUnidomOccupations < ActiveRecord::Migration
       t.text :instruction
 
       t.column   :state, 'char(1)', null: false, default: 'C'
-      t.datetime :opened_at,        null: false, default: ::Time.utc(1970)
+      t.datetime :opened_at,        null: false, default: Time.utc(1970)
       t.datetime :closed_at,        null: false, default: ::Time.utc(3000)
       t.boolean  :defunct,          null: false, default: false
       t.jsonb    :notation,         null: false, default: {}
