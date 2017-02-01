5f72aac774e8221180089d5979a299aed996cc42
Topbit Du <topbit.du@gmail.com>
2017-02-01 19:07:57 +0800

    1, Models RSpec examples manifest.

---
+++ lib/unidom/position/models_rspec.rb
@@ -0,0 +1,4 @@
+require 'rspec/models/unidom/position/occupation_spec'               unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Position::Cccupation'
+require 'rspec/models/unidom/position/position_spec'                 unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Position::Position'
+require 'rspec/models/unidom/position/post_spec'                     unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Position::Post'
+require 'rspec/models/unidom/position/post_reporting_structure_spec' unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Position::PostReportingStructure'
