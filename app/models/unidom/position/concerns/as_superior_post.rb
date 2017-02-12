f19e304960c4c655ec0b37e3b5585c0ea7d8f56e
Topbit Du <topbit.du@gmail.com>
2017-02-12 09:47:31 +0800

    1, Improve the As Superior Post concern for the document.

--- app/models/unidom/position/concerns/as_superior_post.rb
+++ app/models/unidom/position/concerns/as_superior_post.rb
@@ -10,6 +10,10 @@ module Unidom::Position::Concerns::AsSuperiorPost
     has_many :inferior_post_reporting_structures, class_name: 'Unidom::Position::PostReportingStructure', source: :superior_post, foreign_key: :superior_post_id
     has_many :inferior_posts,                     through:    :inferior_post_reporting_structures,        source: :inferior_post
 
+    ##
+    # 以上级岗位的身份与下级岗位 by 建立岗位间的上下级汇报关系。
+    # 主要汇报标志是 primary ，缺省为 true 。建立时间是 at ，缺省为当前时间。如：
+    # project_manager.is_reported_to! by: rails_developer, primary: false
     def is_reported_to!(by: nil, at: Time.now, primary: true)
       inferior_post_reporting_structures.inferior_post_is(by).valid_at(now: at).alive.first_or_create! elemental: primary, opened_at: at
     end
