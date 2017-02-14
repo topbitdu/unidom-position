a437a375162ba9a9821f92f1e8dbe7351199ea31
Topbit Du <topbit.du@gmail.com>
2017-02-14 09:59:18 +0800

    1, Improve the Occupation spec for the validations on the #name attribute.

--- lib/rspec/models/unidom/position/occupation_spec.rb
+++ lib/rspec/models/unidom/position/occupation_spec.rb
@@ -15,7 +15,20 @@ describe Unidom::Position::Occupation, type: :model do
       name:        'Some Occupation'
     }
 
+    name_max_length = described_class.columns_hash['name'].limit
+
     it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
+
+    it_behaves_like 'validates', model_attributes, :name,
+      {             } => 0,
+      { name: nil   } => 2,
+      { name: ''    } => 2,
+      { name: 'A'   } => 1,
+      { name: 'AA'  } => 0,
+      { name: 'AAA' } => 0,
+      { name: 'A'*(name_max_length-1) } => 0,
+      { name: 'A'*name_max_length     } => 0,
+      { name: 'A'*(name_max_length+1) } => 1
 
   end
 
