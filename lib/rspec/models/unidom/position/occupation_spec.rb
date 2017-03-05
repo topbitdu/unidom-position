6a176dbc6597380ee90fd3eef56ff634be0bdc12
Topbit Du <topbit.du@gmail.com>
2017-03-05 20:04:37 +0800

    1, Improve the Occupation spec for the scheme_id_is scope, & the scheme_type_is scope.

--- lib/rspec/models/unidom/position/occupation_spec.rb
+++ lib/rspec/models/unidom/position/occupation_spec.rb
@@ -44,6 +44,38 @@ describe Unidom::Position::Occupation, type: :model do
 
     it_behaves_like 'has_many', model_attributes, :positions, Unidom::Position::Position, [ position_1_attributes, position_2_attributes ]
 
+=begin
+    post_1_attributes = {
+      organization_id:   SecureRandom.uuid,
+      organization_type: 'Unidom::Position::Organization::Mock',
+      #position_id:       SecureRandom.uuid,
+      name:              'Some Post #1'
+    }
+
+    post_2_attributes = {
+      organization_id:   SecureRandom.uuid,
+      organization_type: 'Unidom::Position::Organization::Mock',
+      #position_id:       SecureRandom.uuid,
+      name:              'Some Post #2'
+    }
+
+    it_behaves_like 'has_many', model_attributes, :posts, Unidom::Position::Post, [ post_1_attributes, post_2_attributes ]
+=end
+
+    it_behaves_like 'scope', :scheme_id_is, [
+      { attributes_collection: [ model_attributes                                             ], count_diff: 1, args: [ model_attributes[:scheme_id] ] },
+      { attributes_collection: [ model_attributes                                             ], count_diff: 0, args: [ Unidom::Common::NULL_UUID    ] },
+      { attributes_collection: [ model_attributes.merge(scheme_id: Unidom::Common::NULL_UUID) ], count_diff: 0, args: [ model_attributes[:scheme_id] ] },
+      { attributes_collection: [ model_attributes.merge(scheme_id: Unidom::Common::NULL_UUID) ], count_diff: 1, args: [ Unidom::Common::NULL_UUID    ] }
+    ]
+
+    it_behaves_like 'scope', :scheme_type_is, [
+      { attributes_collection: [ model_attributes                                                       ], count_diff: 1, args: [ model_attributes[:scheme_type]    ] },
+      { attributes_collection: [ model_attributes                                                       ], count_diff: 0, args: [ 'Unidom::Position::Scheme::Mock2' ] },
+      { attributes_collection: [ model_attributes.merge(scheme_type: 'Unidom::Position::Scheme::Mock2') ], count_diff: 0, args: [ model_attributes[:scheme_type]    ] },
+      { attributes_collection: [ model_attributes.merge(scheme_type: 'Unidom::Position::Scheme::Mock2') ], count_diff: 1, args: [ 'Unidom::Position::Scheme::Mock2' ] }
+    ]
+
   end
 
 end
