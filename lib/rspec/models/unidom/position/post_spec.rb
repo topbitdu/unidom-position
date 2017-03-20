e70e56c5d5c6a2e0bb4d84461b228cc00202ab3b
Topbit Du <topbit.du@gmail.com>
2017-03-20 16:30:31 +0800

    1, Improve the Post spec for the ``belongs_to :position, class_name: 'Unidom::Position::Position'`` macro.

--- lib/rspec/models/unidom/position/post_spec.rb
+++ lib/rspec/models/unidom/position/post_spec.rb
@@ -19,6 +19,15 @@ describe Unidom::Position::Post, type: :model do
 
     it_behaves_like 'validates text', model_attributes, :name, length: 2..described_class.columns_hash['name'].limit
 
+    position_attributes = {
+      organization_id:   SecureRandom.uuid,
+      organization_type: 'Unidom::Position::Organization::Mock',
+      occupation_id:     SecureRandom.uuid,
+      name:              'Some Position'
+    }
+
+    it_behaves_like 'belongs_to', model_attributes, :position, Unidom::Position::Position, position_attributes
+
   end
 
 end
